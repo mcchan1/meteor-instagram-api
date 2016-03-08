169b493ff2708ca500c0448567909c09dee23e8c
mcchan1 <mcchan1@gmail.com>
2016-03-08 12:42:33 -0500

    call from client to server test

--- client/instagramMeteor.js
+++ client/instagramMeteor.js
@@ -5,7 +5,14 @@ if (Meteor .isClient){
 
 Template.hashtag.events({
 	"submit .hashtagSearch": function (event) {
-		
+		event.preventDefault();
+
+		// var input = event.target.text.value;
+
+		// $('#submit').click(function() {
+		// 	loadPictures();
+		// })
+		Meteor.call('ok');
 	}
 })
 
@@ -13,11 +20,13 @@ Template.hashtag.events({
 //could also put into onwn javascript file e.g. instafeed.js 
 Template.instafeed.helpers({
 	'headline': function () {
-		return "some bullshit headline"
+		return "some  headline"
+
 	},
 
 	'loadPictures' : function () {
-	var tag = 'qoobear';//input.val(); //assign the input value to 'tag'
+	
+	var tag = 'qoobear'//input.val(); //assign the input value to 'tag'
 
 	var api_key = '1634185146.5b9e1e6.ebf4b224796843379782a86ea0664c24'
 
@@ -28,8 +37,32 @@ Template.instafeed.helpers({
 		dataType: "jsonp",
 		cache: false,
 		url: url,
-	 	success: console.log('instagram'),
-		
+	 	success: function(data) {
+		 	photos = data.data; 
+			if (photos.length > 0) {
+
+			  $('.instagramPhoto').html(''); 
+
+			 for (var i = 0; i < 6; i++) {
+			      $(".instagramPhoto").append(
+			      	'<div class = "instagram">' + 
+			      	'<div class = "user">' + 
+			      	'<img src="' + photos[i].user.profile_picture + ' " />' + 
+			      	'<h1>' + photos[i].user.username + '</h1>' + 
+			      	'</div>' + 
+			      	'<img src="' + photos[i].images.low_resolution.url + '"/>' +
+			      	'</div'
+				)} //for loop
+			} //if
+			else {
+				$('.instagramPhoto').html('no pics with that #');
+			}
+
+			$(".instagram").click(function(){
+				$(this).remove();
+			})
+
+		 	},	//sucess
 	}); //ajax call 
 
 	} //load pictures
--- server/main.js
+++ server/main.js
@@ -1,14 +1,15 @@
 if(Meteor.isServer){
 	console.log('hello server');
 
-	// Meteor.startup(function (){
-	// 	var feed = new Instafeed({
- //        get: 'tagged',
- //        tagName: 'awesome',
- //        clientId: 'a2df9087998b4356b28162781ccd2da1'
- //    });
- //    feed.run();
+	Meteor.methods ({
+		ok: function () {
+			
+			for (var i = 0; i< 5; i++){
+				console.log("ok");
+				
+			}
+		}
 
-	// });
+	});
 
 }
