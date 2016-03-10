29b86a1819f2645e4354cc0e01811c13010b3976
mcchan1 <mcchan1@gmail.com>
2016-03-10 09:24:57 -0500

    server side, tag, access token

--- server/server.js
+++ server/server.js
@@ -4,12 +4,11 @@ if(Meteor.isServer){
 	Meteor.methods({
 		searchInstagram: function () {
 			console.log('checking..');
-			
-			HTTP.call( 'GET', 'https://api.instagram.com/v1/tags/qoobear/media/recent?access_token=1634185146.1677ed0.d05110c153ab4f86b27f2e99d58a3f3c', {
+			tag = 'qoobear';
+			HTTP.call( 'GET', 'https://api.instagram.com/v1/tags/'+tag+'/media/recent?access_token=1634185146.1677ed0.d05110c153ab4f86b27f2e99d58a3f3c', {
 				params: {
-					// 'ACCESS_TOKEN': '1634185146.1677ed0.d05110c153ab4f86b27f2e99d58a3f3c',
-					 'COUNT': 1
-
+					  	
+				  'count': 1,
 				}
 			}, function( error, data ) {
 
@@ -18,16 +17,8 @@ if(Meteor.isServer){
 			  } 
 			  else {
 			  	photos = data.data;
-			    console.log( photos);
-			    /*
-			     This will return the HTTP response object that looks something like this:
-			     {
-			       content: "String of content...",
-			       data: Array[100], <-- Our actual data lives here. 
-			       headers: {  Object containing HTTP response headers }
-			       statusCode: 200
-			     }
-			    */
+			    
+			    console.log(photos)
 			  }
 			});
 		}, 
@@ -40,39 +31,5 @@ if(Meteor.isServer){
 
 	// var url = 'https://api.instagram.com/v1/tags/'+ tag +'/media/recent?access_token='+api_key
 
-	// $.ajax({
-	// 	type: "GET",
-	// 	dataType: "jsonp",
-	// 	cache: false,
-	// 	url: url,
-	//  	success: function(data) {
-	// 	 	photos = data.data; 
-	// 		if (photos.length > 0) {
-
-	// 		  $('.instagramPhoto').html(''); 
-
-	// 		 for (var i = 0; i < 6; i++) {
-	// 		      $(".instagramPhoto").append(
-	// 		      	'<div class = "instagram">' + 
-	// 		      	'<div class = "user">' + 
-	// 		      	'<img src="' + photos[i].user.profile_picture + ' " />' + 
-	// 		      	'<h1>' + photos[i].user.username + '</h1>' + 
-	// 		      	'</div>' + 
-	// 		      	'<img src="' + photos[i].images.low_resolution.url + '"/>' +
-	// 		      	'</div'
-	// 			)} //for loop
-	// 		} //if
-	// 		else {
-	// 			$('.instagramPhoto').html('no pics with that #');
-	// 		}
-
-	// 		$(".instagram").click(function(){
-	// 			$(this).remove();
-	// 		})
-
-	// 	 	},	//sucess
-	// }); //ajax call 
-
-	// } //load pictures
 	}); //methods
 }
