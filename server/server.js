91d4593f6cb8ac6f64d222a4464e90c28124bd69
mcchan1 <mcchan1@gmail.com>
2016-03-23 14:49:16 -0400

    Removed personal info

--- server/server.js
+++ server/server.js
@@ -6,23 +6,23 @@ if(Meteor.isServer){
 		console.log("startup server function");
 
 	});
-
+//PUBLICATIONS 
 	Meteor.publish('instafeed', function publishFunction(){
 		//wait 2s before loading data
 		Meteor._sleepForMs(2000);
 		console.log('publication ready');
-		//only two fields from Photographs Collection for publication -'text' and 'url'
+		//only two fields from Photographs Collection will be available for publication -'text' and 'url'
 		return Photographs.find({},{fields: {"data.caption.text":1,"data.images.low_resolution.url":1} });
 		
 	}); //end of publish
-
+//METHODS 
 	Meteor.methods({
 		//search instagram, using http-request package 
 		//Use Session variable 'hashtagIdVar' for the 'tag' value in instagram
 		searchInstagram: function (hashtagIdVar) {
 			console.log('checking instagram...');
 
-			HTTP.call( 'GET', 'https://api.instagram.com/v1/tags/'+hashtagIdVar+'/media/recent?access_token=1634185146.1677ed0.d05110c153ab4f86b27f2e99d58a3f3c', {
+			HTTP.call( 'GET', 'https://api.instagram.com/v1/tags/'+hashtagIdVar+'/media/recent?access_token=YOUR-ACCESS-TOKEN', {
 				params: {
 					  	
 				  'count': 2, //return two instagram posts
