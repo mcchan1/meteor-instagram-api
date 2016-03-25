5f03b373c1f8c75d1ae2fe5a8fb5fe42ea57b5a5
mcchan1 <mcchan1@gmail.com>
2016-03-25 15:58:00 -0400

    collection comments

--- collections.js
+++ collections.js
@@ -1,2 +1,4 @@
 Photographs = new Mongo.Collection('photographs');
-Hashtag = new Mongo.Collection('hashtag');
+//All the data gathered from instagram post will be collected. 
+//Publication will filter out data to be made available to the Subsctiption
+//loadPictures function will choose data from the Subsctiption to be displayed in template
